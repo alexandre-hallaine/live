08e90ab678773e55ca431edaf6968bb87db2363d
Alexandre Hallaine <alexandre@hallaine.com>
2025-06-18 23:54:24 -0400

    feat: Forces output video to 1920x1080 resolution

--- render.sh
+++ render.sh
@@ -13,6 +13,7 @@ DUR=$(ffprobe -v error -show_entries format=duration -of csv=p=0 audio.m4a)
 
 ffmpeg -y -stream_loop -1 -i "$2" -i audio.m4a \
   -map 0:v -map 1:a -t "$DUR" \
+  -vf "scale=1920:1080:flags=lanczos" \
   -c:v libx264 -preset fast -pix_fmt yuv420p -c:a aac "$3"
 
 rm audio.m4a
